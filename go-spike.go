8a7d5fd0d33a2147bf115dadcab5e5aa2591bb92
Ramesh Sunkara <ramesh.sunkara@hpe.com>
2021-08-13 11:02:51 -0400

    go spike

--- go-spike.go
+++ go-spike.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rameshsunkara/go-spike-lib"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	fmt.Println(go_spike_lib.Reverse("!oG ,olleH"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rameshsunkara/go-spike
 
 go 1.13
+
+require github.com/rameshsunkara/go-spike-lib v0.0.0-20210813145535-4c48bc61ef0c
